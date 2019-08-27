d01b80ebaa0e0dae34268e95ddd04460e54953bf
Ren Hao <952284692@qq.com>
2019-08-27 16:15:37 +0800

    Edit for level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Map;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Duke {
+//level-1
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        String optionline = "-----------------------------\n";
+        Scanner input  = new Scanner(System.in);
+
+        System.out.println(optionline);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(optionline);
+        String userinput;
+
+        while(!((userinput=input.nextLine()).equals("bye"))){
+            System.out.println(optionline);
+            System.out.println(userinput);
+            System.out.println(optionline);
+        }
+        System.out.println(optionline);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(optionline);
     }
 }
