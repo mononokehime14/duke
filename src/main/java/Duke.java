e0a6ede05f79a2298c540e18403a98a441bcb574
Ren Hao <952284692@qq.com>
2019-08-28 11:50:02 +0800

    Edit for A-Classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Duke {
-    //level-3
+    //level-3 A-Classes
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -14,48 +14,61 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
         String optionline = "-----------------------------\n";
         Scanner input  = new Scanner(System.in);
-        HashMap<String,Integer> Hashlist = new HashMap<String,Integer>();
-
+//        HashMap<String,Integer> Hashlist = new HashMap<String,Integer>();
+        ArrayList<Task> Tasklist = new ArrayList<Task>();
         System.out.println(optionline);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println(optionline);
         String userinput;
-        String checkmark  ="\u2713";
-        String crossmark = "\u2717";
+//        String checkmark  ="\u2713";
+//        String crossmark = "\u2717";
         while(!((userinput=input.nextLine()).equals("bye"))){
             String[] splitstring  = userinput.split(" ");
 
             if(userinput.equals("list")) {
                 System.out.println(optionline);
                 System.out.println("Here are the tasks in your list:");
-                int count = 0;
-                for (Map.Entry<String,Integer> entry : Hashlist.entrySet()) {
-                    count++;
-                    if (entry.getValue() == 0) {
-                        System.out.println(count + "." + " ["+crossmark+"] " + entry.getKey());
-                    } else {
-                        System.out.println(count + "." +" ["+checkmark+"] " + entry.getKey());
-                    }
+                for (int i=0;i<Tasklist.size();i++){
+                    String tick = Tasklist.get(i).getStatusIcon();
+                    System.out.println((i+1)+".[" + tick +"] "+ Tasklist.get(i).description);
                 }
+//                int count = 0;
+//                for (Map.Entry<String,Integer> entry : Hashlist.entrySet()) {
+//                    count++;
+//                    if (entry.getValue() == 0) {
+//                        System.out.println(count + "." + " ["+crossmark+"] " + entry.getKey());
+//                    } else {
+//                        System.out.println(count + "." +" ["+checkmark+"] " + entry.getKey());
+//                    }
+//                }
                 System.out.println(optionline);
             }else if(splitstring[0].equals("done")){
                 System.out.println(optionline);
                 System.out.println("Nice! I've marked this task as done: ");
-                int count = 0;
-                for (Map.Entry<String,Integer> entry : Hashlist.entrySet()) {
-                    count++;
-                    if(count == Integer.parseInt(splitstring[1])){
-                        Hashlist.put(entry.getKey(),1);
-                        System.out.println(" ["+checkmark+"] "+ entry.getKey());
+                for(int i =0 ;i<Tasklist.size();i++){
+                    if(i + 1== Integer.parseInt(splitstring[1])){
+                        Tasklist.get(i).markAsDone();
+                        String tick = Tasklist.get(i).getStatusIcon();
+                        System.out.println("  ["+tick+"] " + Tasklist.get(i).description);
                     }
                 }
+//                int count = 0;
+//                for (Map.Entry<String,Integer> entry : Hashlist.entrySet()) {
+//                    count++;
+//                    if(count == Integer.parseInt(splitstring[1])){
+//                        Hashlist.put(entry.getKey(),1);
+//                        System.out.println(" ["+checkmark+"] "+ entry.getKey());
+//                    }
+//                }
                 System.out.println(optionline);
             }else {
                 System.out.println(optionline);
                 System.out.println("added: " + userinput);
                 System.out.println(optionline);
-                Hashlist.put(userinput,0);
+//                Hashlist.put(userinput,0);
+                Task t = new Task(userinput);
+                Tasklist.add(t);
             }
         }
         System.out.println(optionline);
